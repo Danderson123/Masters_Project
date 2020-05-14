a3f0701f857be01637d58f40ea087fadf529fb36
Danderson123 <43540934+Danderson123@users.noreply.github.com>
2020-05-14 12:26:22 +0100

    Update Entrez.py

--- Entrez.py
+++ Entrez.py
@@ -7,10 +7,12 @@ Entrez.email = "danielanderson1@hotmail.com"
 Accessions = ['NZ_CP027540.1', 'NC_017592.1', 'NC_012469.1', 'NC_011900.1', 'NZ_AKVY01000001.1', 'NC_003098.1']
 search = " ".join(Accessions)
 
+sequences = []
 handle = Entrez.read(Entrez.esearch(db="nucleotide", term=search, retmode="xml"))
 genome_ids = handle['IdList']
 for genome_id in genome_ids:
     record = Entrez.efetch(db="nucleotide", id=genome_id, rettype="fasta", retmode="text")
+    sequences.append(record.read())
     filename = 'genBankRecord_{}.fasta'.format(genome_id)
     print('Writing:{}'.format(filename))
     with open(filename, 'w') as f:
