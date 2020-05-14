db32592946836284838628eb2c09667f175fb747
Danderson123 <43540934+Danderson123@users.noreply.github.com>
2020-05-14 12:17:28 +0100

    Update master.py

--- master.py
+++ master.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 14 11:25:05 2020
-
-@author: danielanderson
-"""
-
-
 import subprocess
 
-f = str((open('D39V.fasta','r').read())) 
+with open('D39V.fasta','r') as f:
+    f = f.read()
+
 # Set up the echo command and direct the output to a pipe
-output = subprocess.Popen(['prokka', f], stdout = subprocess.PIPE).communicate()[0]
+subprocess.run(['prokka', 'D39V.fasta'], capture_output = True)
+
