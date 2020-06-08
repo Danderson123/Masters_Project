b34e51dd4c8e4915afd46508713f981dbed6febe
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2020-06-08 18:20:27 +0100

    Add files via upload

--- scripts/retrieve_sequences.py
+++ scripts/retrieve_sequences.py
@@ -6,6 +6,8 @@ This script downloads fasta and gff files in fasta format for accessions of inte
 import time
 import os
 
+from .integrate import replace_all
+
 def get_options(): #options for downloading and cleaning
     
     import argparse
@@ -23,12 +25,7 @@ def get_options(): #options for downloading and cleaning
                         help='sequences to download, specify "genues species" for all accessions or specific accessions of interest. Separate accessions by "," with no spaces',
                         type=str) #Specify the search term for entrez
      
-   # io_opts.add_argument("--complete",
-                        #  dest="complete",
-                         # help="only retrieve complete genomes",
-                         # action='complete',
-                         # default=None) #Option to only retrieve complete genomes  
-    
+
     io_opts.add_argument("-o",
                         "--output",
                         dest="output_dir",
@@ -46,8 +43,8 @@ def get_options(): #options for downloading and cleaning
     io_opts.add_argument("--number",
                          dest="number",
                          required=False,
-                         help="maximum number of sequences to retrieve (default = 100000)",
-                         default=100000) #Option to only retrieve complete genomes  
+                         help="maximum number of sequences to retrieve",
+                         default=100)
     
     args = parser.parse_args()
     
@@ -84,8 +81,7 @@ def genome_downloader(email, accession, number):
         gff_file = '{}.gff.gz'.format(label)
         fasta_file = '{}.fna.gz'.format(label)
 
-        print('Writing:{}'.format(gff_file))
-        print('Writing:{}'.format(fasta_file))
+        print('Writing:{}'.format(label))
         
         urllib.request.urlretrieve(gff_link, gff_file)
         urllib.request.urlretrieve(fasta_link, fasta_file)
@@ -97,47 +93,110 @@ def genome_downloader(email, accession, number):
     os.chdir("..")
     return files
 
-def replace_all(text, dic):
-    for i, j in dic.items():
-        text = text.replace(i, j)
-    return text
 
-
-def clean_gffs(headers, input_dir, output_dir):
-    import re 
+def split_contigs(headers, input_dir, output_dir):
     
-    gff = input_dir + '/' + headers + '.gff'
-    o = open(gff, 'r')
-    stored = str(o.read()) 
+    for header in headers:
+        d = {"[": "", "]": "", " ": "", "'": ""}
+        header = replace_all(header, d)
+        fasta = input_dir + '/' + header + '.fna'
+        gff = input_dir + '/' + header + '.gff'
+        
+        with open(fasta, 'r') as f:
+            stored_fasta = str(f.read()) 
+            
+        with open(gff, 'r') as g:
+            stored_gff = str(g.read()) 
+            
+        stored_fasta = stored_fasta.split('>')
+        stored_fasta = stored_fasta[1:]
+        
+        for fasta_region in stored_fasta:
+            filename_split = 'contigs' + '/' + fasta_region.split(" ")[0] + ".fna"
+            outfile = open(filename_split,'w')          
+            outfile.write(">" + fasta_region)
+            outfile.close()
+        
+        stored_gff = stored_gff.split("##sequence-region ")
+        stored_gff = stored_gff[1:]
+        
+        for gff_region in stored_gff:
+               filename_split = 'contigs' + '/' + gff_region.split(" ")[0] + ".gff"
+               outfile = open(filename_split,'w')          
+               outfile.write("##sequence-region " + gff_region)
+               outfile.close()
     
-    d = {"Dbxref=": "dbxref=", "Notes=": "note="}
-    stored = replace_all(stored, d) #Ensure GFF annotation format is consistent with prokka output
-    
-    gene_list = stored.splitlines() 
-    title = gene_list[7]
-    gene_list = gene_list[9:-1] #remove non-essential content
-    
-    genes = []
-    positions = []
-    for y in range(len(gene_list)):
-        try:
-            if re.split(r'\t+', gene_list[y])[2] == 'gene':
-                genes.append(gene_list[y])
-        except:
-            print(gene_list[y])
-    
-    cleaned_gffs = "\n".join(str(z) for z in genes)
-    cleaned_gffs = title + '\n' + cleaned_gffs
-
-    #Concatenate downloaded fasta and reformatted GFF
-    fasta_file = input_dir +'/' + headers + '.fna'
-    cleaned_gffs = cleaned_gffs + '\n' + '##FASTA' + '\n' + str((open(fasta_file,'r').read())) 
-    filename_cleaned = output_dir + '/' + headers + '_cleaned.gff'
-    outfile = open(filename_cleaned,'w')          
-    outfile.write(cleaned_gffs)
-    outfile.close()
-
     return
+
+def clean_gffs(input_dir, output_dir):
+    
+    import re 
+    import glob 
+    import os
+    
+    #Import and process annotations
+    path_gff = 'contigs' + "/*.gff"
+    
+    files_gff = glob.glob(path_gff)
+    
+    files_complete = 0
+    
+    for file in files_gff:
+        with open(file,"r") as gff:
+            stored_gff = gff.read()
+       
+        d = {"Dbxref=": "dbxref=", "Notes=": "note="}
+        stored_gff = replace_all(stored_gff, d) #Ensure GFF annotation format is consistent with prokka output
+            
+        gene_list = stored_gff.splitlines() 
+        
+        for line in range(len(gene_list)):
+            if '##sequence-region' in gene_list[line]:
+                title = gene_list[line]
+            elif '###' in gene_list[line]:
+                gene_list = gene_list[:line]
+        
+        gene_list = gene_list[2:]
+        
+        genes = []
+        for y in range(len(gene_list)):
+            split = re.split(r'\t+', gene_list[y])
+            if split[2] == 'CDS' and ((int(split[4]) - int(split[3]) + 1) % 3) == 0: #process_pokka input only looks for CDSs and returns duplicate error when the exon is split.
+                genes.append(gene_list[y])
+            else:
+                pass
+        
+        Ids= []
+        for gene in range(len(genes)):
+            Id = re.search('ID=(.*?);', genes[gene]).group(1)
+            Ids.append(Id)
+        
+        output_cds = []
+        for Id in range(len(Ids)):
+            if not Ids.count(Ids[Id]) == 1:
+                print('Removing split CDS {}'.format(Ids[Id]))
+            else:
+                output_cds.append(genes[Id])
+        
+        if len(output_cds) == 0:
+            continue
+        
+        cleaned_gffs = "\n".join(str(z) for z in output_cds)
+        cleaned_gffs = title + '\n' + cleaned_gffs
+        
+        #Concatenate downloaded fasta and reformatted GFF
+        header = os.path.basename(file).split(".gff")[-2]
+        fasta_file = 'contigs/' + header + '.fna'
+        cleaned_gffs = cleaned_gffs + '\n' + '##FASTA' + '\n' + str((open(fasta_file,'r').read())) 
+        filename_cleaned = output_dir + '/' + header + '.gff'
+        outfile = open(filename_cleaned,'w')          
+        outfile.write(cleaned_gffs)
+        outfile.close()
+        
+        files_complete += 1
+        print(str(files_complete) + "/" + str(len(files_gff)) + " complete")
+    return
+
 
 def main():
     start = time.time()
@@ -148,28 +207,32 @@ def main():
 
     current_dir = os.getcwd() + '/'
     os.makedirs(str(current_dir + 'raw_files'))
+    os.makedirs(str(current_dir + 'contigs'))
     os.makedirs(str(current_dir + args.output_dir))
     accessions = args.accessions.replace('"', "")
     accessions = args.accessions.split(',')
     
+    print("Retrieving Sequences")
+    
     if isinstance(accessions, str):
-        headers = genome_downloader(args.email, accessions, (int(args.number) + 1))
+        headers = genome_downloader(args.email, accessions, int(args.number))
     elif isinstance(accessions, list): 
          headers = []
          for accession in accessions:
              headers.append(str(genome_downloader(args.email, accession, (int(args.number)+1))))#, args.complete)
    
     headers = headers[0].split(',')
-    print(headers)
     print(('found {} ids').format(len(headers)))
     
-    for header in headers:
-        header = header.replace("[","")
-        header = header.replace("]","")
-        header = header.replace("'","")
-        header = header.replace(" ","")
-        clean_gffs(header, 'raw_files', args.output_dir)
+    print("Separating contigs")
     
+    split_contigs(headers,'raw_files', 'contigs')
+    
+    print("Processing GFF files")
+    
+    clean_gffs('contigs', args.output_dir)
+    
+    print("Done")
     end = time.time()
     print(end - start)
     
