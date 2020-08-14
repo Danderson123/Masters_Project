53f440eeef6c59321ab942046eb740ae4fc69436
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2020-08-14 16:54:14 +0100

    Add files via upload

---
+++ build_index_ref.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 17:02:29 2020
+
+@author: danielanderson
+"""
+
+import networkx as nx
+import os 
+from tqdm import tqdm
+import cobs_index as cobs
+
+def long_centroid(graph):
+    """"Translate all alignement files in path and add to single list"""
+    all_proteins = []
+    
+    G = nx.read_gml(graph)
+    for node in G._node:
+        y = G._node[node]
+        seq = y["protein"].split(";")
+        for x in seq:
+            all_proteins.append(">" + y["name"] + "\n" + x)
+            
+    all_proteins = "\n".join(all_proteins)
+    return all_proteins
+
+all_proteins = long_centroid("ATCC_INTEGRATED/final_graph.gml")
+
+os.mkdir("INDEX")
+os.mkdir("FILES")
+
+protein_file = all_proteins.split(">")[1:]
+
+titles = []
+cleaned = []
+for x in protein_file:
+    cleaned.append("".join(x.splitlines()[1:]))
+    titles.append(x.splitlines()[0])
+
+hundred_thousand_indexes = []
+for x in range(len(protein_file)):
+    hundred_thousand_indexes.append(x)
+    
+proteins = []
+gene_names = []
+for protein in hundred_thousand_indexes:
+    info = cleaned[protein]
+    proteins.append(info)
+    gene_names.append(titles[protein])
+
+duplicate_set = set()
+duplicate_count = 1
+for prot_index in tqdm(range(len(hundred_thousand_indexes))):
+    info = proteins[prot_index]
+    if not gene_names[prot_index] in duplicate_set:
+        thou = open("FILES/" + gene_names[prot_index] + ".txt", "w")
+        thou.write(info)
+        thou.close()
+        duplicate_set.add(gene_names[prot_index])
+    else:
+        thou = open("FILES/" + gene_names[prot_index] + ";" + str(duplicate_count) + ".txt", "w")
+        thou.write(info)
+        thou.close()
+        duplicate_count += 1
+        
+params = cobs.CompactIndexParameters()
+params.term_size = 10
+params.clobber = True               # overwrite output and temporary files
+params.false_positive_rate = 0.01    # higher false positive rate -> smaller index
+cobs.compact_construct("FILES/", "INDEX/" + "10_index_index.cobs_compact", index_params=params)
